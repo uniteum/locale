06120de5b68d9a1db3ccedc7aaee3338f8df23fc
Paul Reinholdtsen <paul@bitsy.services>
2026-03-19 16:45:30 -0500

    Deploy USDC to sepolia

--- script/AddressLookupMake.s.sol
+++ script/AddressLookupMake.s.sol
@@ -7,9 +7,10 @@ import {AddressLookup, IUintToAddressMaker} from "../src/AddressLookup.sol";
 
 /// @notice Deploy an AddressLookup clone ONLY if it doesn't already exist (idempotent).
 /// @dev Environment variables (required):
-///   - proto : address of the AddressLookup (IUintToAddressMaker) contract
-///   - config  : path to JSON config file with { env, id, keyValues }
-/// @dev Usage: proto=AddressLookupProto.json config=io/testnet/blocker.json clone=blocker.json forge script script/AddressLookupMake.s.sol -f $chain --private-key $tx_key --broadcast
+///   - env : name of the environment ("prod" or "test")
+///   - chain : id of the chain (e.g. "11155111")
+///   - config  : name of JSON config file with { env, id, keyValues }
+/// @dev Usage: config=USDC.json forge script script/AddressLookupMake.s.sol -f $chain --private-key $tx_key --broadcast
 contract AddressLookupMake is Script {
     struct Config {
         string env;
@@ -20,16 +21,22 @@ contract AddressLookupMake is Script {
     function run() external {
         console2.log("script   : AddressLookupMake");
 
-        string memory dir = string.concat("io/", vm.envString("chain"));
+        string memory indir = string.concat("io/", vm.envString("env"), "/");
+        string memory dir = string.concat(indir, vm.envString("chain"), "/");
+        string memory protoPath = string.concat(dir, "AddressLookupProto.json");
+        string memory configEnv = vm.envString("config");
+        string memory configPath = string.concat(indir, configEnv);
+        string memory clonePath = string.concat(dir, configEnv);
 
         // forge-lint: disable-next-line(unsafe-cheatcode)
-        address proto = abi.decode(vm.parseJson(vm.readFile(string.concat(dir, "/", vm.envString("proto")))), (address));
+        address proto = abi.decode(vm.parseJson(vm.readFile(protoPath)), (address));
         console2.log("proto    :", proto);
 
         // forge-lint: disable-next-line(unsafe-cheatcode)
-        Config memory config = abi.decode(vm.parseJson(vm.readFile(vm.envString("config"))), (Config));
+        Config memory config = abi.decode(vm.parseJson(vm.readFile(configPath)), (Config));
         console2.log("id       :", config.id);
         console2.log("env      :", config.env);
+        console2.log("length   :", config.keyValues.length);
 
         (, address predicted,) = IUintToAddressMaker(proto).made(config.keyValues);
         console2.log("predicted:", predicted);
@@ -43,10 +50,10 @@ contract AddressLookupMake is Script {
             action = "deployed";
         }
 
+        console2.log("action   :", action);
         console2.log("actual   :", actual);
 
-        string memory path = string.concat(dir, "/", vm.envString("clone"));
         vm.createDir(dir, true);
-        vm.writeJson(vm.serializeAddress("tmp", config.id, actual), path);
+        vm.writeJson(vm.serializeAddress("tmp", config.id, actual), clonePath);
     }
 }
