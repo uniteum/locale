50ffc228589e4a52d0813e265e9f667cae6306b0
Paul Reinholdtsen <paul@bitsy.services>
2026-05-02 09:34:29 -0500

    AddressLookup=0xcE1B2489203B66f9C8A0405E47673982e1EBd35B

--- script/AddressLookup.sh
+++ script/AddressLookup.sh
@@ -3,8 +3,8 @@ salt=0x0000000000000000000000000000000000000000000000000000000000000000
 initcode=$(forge inspect AddressLookup bytecode)
 initcodehash=$(cast keccak $initcode)
 echo "initcodehash=$initcodehash"
-input=$(cast concat-hex $salt $initcode)
-printf '%s' "$input" > script/AddressLookup.txt
 AddressLookup=$(cast create2 --deployer $deployer --salt $salt --init-code $initcode)
 echo "AddressLookup=$AddressLookup"
-forge verify-contract $AddressLookup AddressLookup --verifier etherscan --show-standard-json-input | jq '.'> script/AddressLookup.json
+input=$(cast concat-hex $salt $initcode)
+printf '%s' "$input" > io/AddressLookup/$AddressLookup.txt
+forge verify-contract $AddressLookup AddressLookup --verifier etherscan --show-standard-json-input | jq '.'> io/AddressLookup/$AddressLookup.json
